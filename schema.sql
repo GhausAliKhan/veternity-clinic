169845f885f30c1207a5abceb4376081910ed073
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-10-18 16:28:02 +0500

    set primary key and not null to id column

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals (
-  id INT, name CHAR(40), 
+  id INT NOT NULL PRIMARY KEY, name CHAR(40), 
   date_of_birth DATE, 
   escape_attempts INT, 
   neutered BOOLEAN, 
