e37580209f5e9bb99138038947abd8b7ee148185
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-10-19 16:33:28 +0500

    create owners table and species table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE vet_clinic;
 
+-- Create the animals table --
 CREATE TABLE animals (
   id SERIAL NOT NULL PRIMARY KEY, name CHAR(40), 
   date_of_birth DATE, 
@@ -8,5 +9,19 @@ CREATE TABLE animals (
   weight_kg DECIMAL
 );
 
--- Add new column species -- 
-ALTER TABLE animals ADD COLUMN species CHAR(40);
+-- Create the owners table --
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(50),
+  age INT
+);
+
+-- Create the species table --
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50)
+);
+
+-- Modify the animals table to add foreign key references --
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
